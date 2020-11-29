2ccd89850e9fd86f39b755b175e1b9fbf1c094a2
Javed Hussain <javedh.dev@gmail.com>
2020-11-29 20:41:25 +0530

    Enhancements

--- app/src/main/java/tech/zenex/habits/LoginActivity.java
+++ app/src/main/java/tech/zenex/habits/LoginActivity.java
@@ -16,97 +16,23 @@ package tech.zenex.habits;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
 public class LoginActivity extends AppCompatActivity {
-    private final int RC_SIGN_IN = 1001;
-    private FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.getAccessToken(true).addOnCompleteListener(tokenTask -> {
-            if (!tokenTask.isSuccessful()) {
-                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestIdToken(getString(R.string.default_web_client_id))
-                        .requestEmail()
-                        .build();
-                mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-                signIn();
-                openMainActivity();
-            } else {
-                openMainActivity();
-            }
-        });
+        new Handler().postDelayed(() -> openMainActivity(), 2000);
 
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     private void openMainActivity() {
         Intent i = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(i);
-//        finish();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d("GSO", "firebaseAuthWithGoogle:" + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w("GSO", "Google sign in failed", e);
-                // ...
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d("GSO", "signInWithCredential:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("GSO", "signInWithCredential:failure", task.getException());
-                        Toast.makeText(getApplicationContext(), "Authentication Failed.",
-                                Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
-                    }
-
-                    // ...
-                });
+        finish();
     }
 }
--- app/src/main/java/tech/zenex/habits/SettingsActivity.java
+++ app/src/main/java/tech/zenex/habits/SettingsActivity.java
@@ -17,6 +17,7 @@ package tech.zenex.habits;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -25,10 +26,24 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import tech.zenex.habits.dialogs.SettingsFragment;
 
 public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    private final int RC_SIGN_IN = 1001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 //        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES)
@@ -66,7 +81,19 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals("backup")) {
-            Toast.makeText(getApplicationContext(), "Changing backup option", Toast.LENGTH_SHORT).show();
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.getAccessToken(true).addOnCompleteListener(tokenTask -> {
+                if (!tokenTask.isSuccessful()) {
+                    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestIdToken(getString(R.string.default_web_client_id))
+                            .requestEmail()
+                            .build();
+                    mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+                    signIn();
+                } else {
+                   Log.d("GSO", "Signed in successfully.");
+                }
+            });
         }/*else if (key.equals("dark_mode")) {
             if(sharedPreferences.getBoolean("dark_mode",false)){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
@@ -82,5 +109,57 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
         Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    /*private void openMainActivity() {
+        Intent i = new Intent(SettingsActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }*/
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("GSO", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("GSO", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("GSO", "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("GSO", "signInWithCredential:failure", task.getException());
+                        Toast.makeText(getApplicationContext(), "Authentication Failed.",
+                                Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                    }
+
+                    // ...
+                });
     }
 }
